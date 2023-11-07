f8e56f04f88b81394e4462cd3192c83fda08590b
unknown <piotrzywczak33@gmail.com>
2023-11-07 13:36:23 +0100

    sth

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-echo 'Hi there 👋';
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Ta struna to bydzie ło niczym">
+        <meta name="keywords" content="strona, ło, niczem">
+        <meta name="author" content="Piotr Żywczak">
+        <link rel="icon" type="image/x-icon" href="image.png">
+        <link rel="stylesheet" href="style.css">
+        <title>Tu bydzie tytuł</title>
+    </head>
+    <body>
+        <?php
+            echo 'Hello there 👋<br>
+                Nobody expects the Spanish Inquisition';
+        ?>
+    </body>
+</html>
